f4939b404cd60f88a37c855753070eb9cabb41c0
Claudio Almeida Vidal <cv256@hotmail.com>
2024-01-04 15:17:45 +0000

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,154 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtFrom = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox1 = New System.Windows.Forms.MaskedTextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lbTest = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(101, 40)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(49, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "from time"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(204, 40)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(38, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "to time"
+        '
+        'txtFrom
+        '
+        Me.txtFrom.BeepOnError = True
+        Me.txtFrom.Location = New System.Drawing.Point(156, 37)
+        Me.txtFrom.Mask = "00:00"
+        Me.txtFrom.Name = "txtFrom"
+        Me.txtFrom.Size = New System.Drawing.Size(42, 20)
+        Me.txtFrom.TabIndex = 2
+        Me.txtFrom.Text = "0000"
+        Me.txtFrom.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.txtFrom.ValidatingType = GetType(Date)
+        '
+        'MaskedTextBox1
+        '
+        Me.MaskedTextBox1.BeepOnError = True
+        Me.MaskedTextBox1.Location = New System.Drawing.Point(248, 37)
+        Me.MaskedTextBox1.Mask = "00:00"
+        Me.MaskedTextBox1.Name = "MaskedTextBox1"
+        Me.MaskedTextBox1.Size = New System.Drawing.Size(42, 20)
+        Me.MaskedTextBox1.TabIndex = 3
+        Me.MaskedTextBox1.Text = "2359"
+        Me.MaskedTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.MaskedTextBox1.ValidatingType = GetType(Date)
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(392, 35)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Test"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(15, 39)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(70, 17)
+        Me.CheckBox1.TabIndex = 6
+        Me.CheckBox1.Text = "Say Time"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(67, 8)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(400, 21)
+        Me.ComboBox1.TabIndex = 7
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 11)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(42, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Lingua:"
+        '
+        'lbTest
+        '
+        Me.lbTest.Location = New System.Drawing.Point(325, 40)
+        Me.lbTest.Name = "lbTest"
+        Me.lbTest.Size = New System.Drawing.Size(61, 13)
+        Me.lbTest.TabIndex = 9
+        Me.lbTest.Text = "99:99"
+        Me.lbTest.TextAlign = System.Drawing.ContentAlignment.TopRight
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(479, 65)
+        Me.Controls.Add(Me.lbTest)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.MaskedTextBox1)
+        Me.Controls.Add(Me.txtFrom)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Speaking Clock CV"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Minimized
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtFrom As MaskedTextBox
+    Friend WithEvents MaskedTextBox1 As MaskedTextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lbTest As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,67 @@
+﻿Public Class Form1
+
+    Private s As System.Speech.Synthesis.SpeechSynthesizer
+    Private Timer1 As New Windows.Forms.Timer()
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Timer1.Interval = 30500
+        Timer1.Enabled = False
+        AddHandler Timer1.Tick, AddressOf Timer1_Tick
+
+        s = New System.Speech.Synthesis.SpeechSynthesizer
+        For Each a As Speech.Synthesis.InstalledVoice In s.GetInstalledVoices()
+            'If not a.Enabled Then Continue for
+            ComboBox1.Items.Add(a.VoiceInfo.Description)
+            If Not String.IsNullOrEmpty(My.Settings.Language) AndAlso a.VoiceInfo.Description = My.Settings.Language Then ComboBox1.SelectedIndex = ComboBox1.Items.Count - 1
+        Next
+
+        CheckBox1.Checked = My.Settings.Enabled
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs)
+        With Now
+            lbTest.Text = .Hour & ":" & .Minute.ToString("00")
+            If .Minute = 0 OrElse .Minute = 30 OrElse .Minute = 15 OrElse .Minute = 45 Then SayTime()
+        End With
+    End Sub
+
+    Private Sub SayTime()
+        With Now
+            Dim descr As String = ComboBox1.SelectedItem
+            Dim a As Speech.Synthesis.InstalledVoice
+            If Not String.IsNullOrEmpty(descr) Then
+                a = s.GetInstalledVoices().FirstOrDefault(Function(f) f.VoiceInfo.Description.Equals(descr))
+                My.Settings.Language = descr
+                My.Settings.Save()
+            Else
+                a = s.GetInstalledVoices().FirstOrDefault(Function(f) f.VoiceInfo.Culture.Equals(Globalization.CultureInfo.CurrentCulture))
+            End If
+            If a IsNot Nothing Then
+                s.SelectVoice(a.VoiceInfo.Name)
+                s.Volume = 100
+                s.Rate = -2
+                If a.VoiceInfo.Culture.Name.StartsWith("pt") Then
+                    s.Rate = -3
+                    s.SpeakAsync(.Hour & If(.Minute = 0, " horas", ",  E " & .Minute))
+                Else
+                    If .Minute = 0 Then
+                        s.SpeakAsync(.Hour & " o clock")
+                    Else
+                        s.SpeakAsync(.Minute & " past " & .Hour)
+                    End If
+                End If
+            End If
+        End With
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        SayTime()
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        Timer1.Enabled = CheckBox1.Checked
+        My.Settings.Enabled = CheckBox1.Checked
+        My.Settings.Save()
+    End Sub
+
+End Class
